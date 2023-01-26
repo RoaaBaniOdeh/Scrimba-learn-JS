a455ec6bcf9ff5dcc326d21d296bdcf2bf121752
RoaaBaniOdeh <rooa.mohye@gmail.com>
2023-01-26 22:35:36 +0200

    mmm

--- build a Chrome Extension/main.js
+++ build a Chrome Extension/main.js
@@ -6,21 +6,66 @@ const ulEl = document.getElementById("ul-el")
 const inputBtn = document.getElementById("input-btn")
 
 
+const tabBtn = document.getElementById("tab-btn")
+
+//turning the "myLeads" string into an array using  Json.parse
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+
+
+
+let render = (leads)=>{
+    let listItems = ""
+    for(let index in leads)
+       // listItems += "<li><a target='_blank' href='#'>" +  myLeads[index] + "</a></li>"
+       listItems += `<li><a target='_blank' href='${leads[index]}'>   ${leads[index]}  </a></li>`
+        ulEl.innerHTML = listItems
+}
+
+//checking if leads is truthy =>if so set myleads to its value and call renderLeads 
+//=> reload the page the myleads that we render stays there => what a quote"persisted our leads across reload"
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+//refactoring the fun. so that it takes a param. ,leads, and use it instead of the global myLeads variable . 
+let TabBtn = ()=> {    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+}
+tabBtn.addEventListener("click", TabBtn)
+
+
+
+
+
+
 let saveLead = ()=> {
     myLeads.push(inputEl.value)
     inputEl.value = "  "
-    renderLeads()
+    //turning the array into an sting again using  Json.stringify
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+   //console.log(localStorage.getItem("myLeads"))
 }
 
 
+let deleteAllLeads = ()=>{
+   console.log("deleteeeeeeeee by double click")
+    localStorage.clear()
+    myLeads = []
 
-let renderLeads = ()=>{
-    let listItems = ""
-    for(let index in myLeads)
-       // listItems += "<li><a target='_blank' href='#'>" +  myLeads[index] + "</a></li>"
-       listItems += `<li><a target='_blank' href='${myLeads[index]}'>   ${myLeads[index]}  </a></li>`
-       
-        ulEl.innerHTML = listItems
-}
+    render(myLeads)
+} 
+
+
+
+//let deleteBtn = document.getElementById("delete-btn")
+document.getElementById("delete-btn").addEventListener("click",deleteAllLeads)
 
 inputBtn.addEventListener("click",saveLead)
